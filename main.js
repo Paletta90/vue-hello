9e596164d006e93726980928674b988cc0653db1
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-30 14:18:05 +0200

    Bonus

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var app = new Vue(
         el: '#root',
         
         data:{
-            title: 'Hello World'
+            title: 'Hello World',
+            image: './img-leon.jpg',
+            width: 'width_100',
+            f_center: 'text-center'
         }
     }
 )
